fa0cafa29be45f86c85e293045c3cedcd10ff010
Up <upcraftedlp@gmail.com>
2023-11-12 11:35:36 +0100

    fix image tags

--- components/Entry.tsx
+++ components/Entry.tsx
@@ -13,9 +13,9 @@ export interface EntryProps {
 export default function Entry({name, logo, link, description} : EntryProps): ReactElement {
 
     return (
-        <div className="text-center text-primary m-2 p-2 md:mb-5 md:p-5 border border-primary rounded-lg bg-secondary bg-opacity-40 shadow-lg">
+        <div className="p-2 m-2 text-center border rounded-lg shadow-lg text-primary md:mb-5 md:p-5 border-primary bg-secondary bg-opacity-40">
 
-            <Image src={logo} className="w-48 h-48 m-auto glowyimg" width="192px" height="192px" alt={name}/>
+            <Image src={logo} alt={name} className="w-48 h-48 m-auto glowyimg" width={192} height={192} />
 
             <div className="text-center">
                 <h3><NavLink title={name} location={link} inline={true}/></h3>
--- components/Sponsor.tsx
+++ components/Sponsor.tsx
@@ -13,9 +13,9 @@ export interface SponsorProps {
 export default function Sponsor({name, logo, link, description} : SponsorProps): ReactElement {
 
     return (
-        <div className="text-center text-primary m-2 p-2 md:mb-5 md:p-5 border border-primary rounded-lg bg-secondary bg-opacity-40 shadow-lg">
+        <div className="p-2 m-2 text-center border rounded-lg shadow-lg text-primary md:mb-5 md:p-5 border-primary bg-secondary bg-opacity-40">
 
-            <Image src={`/sponsors/${logo}`} className="w-48 h-48 m-auto glowyimg" width="192px" height="192px" alt={name}/>
+            <Image src={`/sponsors/${logo}`} className="w-48 h-48 m-auto glowyimg" width={192} height={192} alt={name}/>
 
             <div className="text-center">
                 <h3><NavLink title={name} location={link} inline={true}/></h3>
