0860cbe3209d394eaf273ac70d762ecb055e4638
Up <upcraftedlp@gmail.com>
2023-11-12 11:36:10 +0100

    make entry images load lazily

--- components/Entry.tsx
+++ components/Entry.tsx
@@ -15,7 +15,7 @@ export default function Entry({name, logo, link, description} : EntryProps): Rea
     return (
         <div className="p-2 m-2 text-center border rounded-lg shadow-lg text-primary md:mb-5 md:p-5 border-primary bg-secondary bg-opacity-40">
 
-            <Image src={logo} alt={name} className="w-48 h-48 m-auto glowyimg" width={192} height={192} />
+            <Image src={logo} alt={name} className="w-48 h-48 m-auto glowyimg" width={192} height={192} loading="lazy" />
 
             <div className="text-center">
                 <h3><NavLink title={name} location={link} inline={true}/></h3>
