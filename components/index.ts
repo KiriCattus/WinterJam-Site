3a188da98f5fc43ee2fd27df43496f98907efe6a
Up <upcraftedlp@gmail.com>
2023-11-17 19:55:04 +0100

    switch to SvelteKit

--- components/index.ts
+++
@@ -1,5 +0,0 @@
-export { Title } from "./Title";
-export { SnowParticles } from "./SnowParticles";
-export { Layout } from "./Layout";
-export { NavBar } from "./NavBar";
-export { TimeTable } from "./TimeTable";
