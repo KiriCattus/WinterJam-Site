d5a1324008b7da4638c155dbefa42727159d1738
Up <upcraftedlp@gmail.com>
2023-11-25 07:15:25 +0100

    fix active url store not being propagated

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,9 +1,8 @@
 <script>
-	import { page } from '$app/stores';
 	import classNames from 'classnames';
 	import { Navbar, NavBrand, NavUl, NavHamburger } from 'flowbite-svelte';
 
-	$: activeUrl = $page.url.pathname;
+	export let activeUrl = '';
 
 	const textClasses =
 		'font-bold text-primary text-xl hover:underline focus:underline hover:text-important focus:text-important';
@@ -11,11 +10,15 @@
 
 <Navbar color="none">
 	<NavBrand href="/">
-		<enhanced:img src="$assets/sponsors/mmd/logo-winter.png" alt="MMD Winter Logo" class="w-12 h-12" />
+		<enhanced:img
+			src="$assets/sponsors/mmd/logo-winter.png"
+			alt="MMD Winter Logo"
+			class="w-12 h-12"
+		/>
 	</NavBrand>
 	<NavHamburger />
 	<NavUl
-		{activeUrl}
+		bind:activeUrl
 		activeClass={classNames(textClasses, 'underline')}
 		nonActiveClass={textClasses}
 	>
