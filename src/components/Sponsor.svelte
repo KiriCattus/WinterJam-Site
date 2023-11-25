ce80e46332dfdbbe9c0c860ac6b0cb495ee96206
Up <upcraftedlp@gmail.com>
2023-11-25 06:41:41 +0100

    fix sponsor cards

--- src/components/Sponsor.svelte
+++ src/components/Sponsor.svelte
@@ -1,27 +1,32 @@
 <script lang="ts">
 	import classNames from 'classnames';
-import { Card } from 'flowbite-svelte';
 
 	export let name: string;
 	export let logo: string | undefined = undefined;
 	export let href: string | undefined = undefined;
 	export let target: string | undefined = '_blank';
 	export let rel: string | undefined = 'noopener noreferrer';
-    export let reverse: boolean = true;
+	export let reverse: boolean = true;
 </script>
 
-<Card {href} {target} {rel} title={name} horizontal>
-	<div class={classNames("flex gap-4 min-w-max", reverse ? 'flex-row-reverse' : 'flex-row')}>
-		<enhanced:img
-			src={logo}
-			alt={name}
-			class="p-1 ring-2 ring-gray-300 dark:ring-gray-500 w-32 h-32 bg-gray-100 dark:bg-gray-600 text-gray-600 dark:text-gray-300 rounded-full"
-		/>
-		<div class="max-w-sm flex items-center">
-			<span class="inline-block text-center tracking-tight">
-				<slot />
-			</span>
-		</div>
+<a
+	{href}
+	{target}
+	{rel}
+	title={name}
+	class={classNames(
+		'bg-white dark:bg-gray-800 text-gray-500 dark:text-gray-400 rounded-lg border border-gray-200 dark:border-gray-700 divide-gray-200 dark:divide-gray-700 shadow-md flex max-w-sm md:max-w-xl hover:bg-gray-100 dark:hover:bg-gray-700 mb-4 flex-col p-6 items-center gap-2',
+		reverse ? 'md:flex-row-reverse' : 'md:flex-row'
+	)}
+>
+	<enhanced:img
+		src={logo}
+		alt={name}
+		class="p-1 ring-2 ring-gray-300 dark:ring-gray-500 w-32 h-32 bg-gray-100 dark:bg-gray-600 text-gray-600 dark:text-gray-300 rounded-full m-auto"
+	/>
+	<div class="max-w-sm flex items-center">
+		<span class="inline-block text-center tracking-tight">
+			<slot />
+		</span>
 	</div>
-</Card>
-
+</a>
