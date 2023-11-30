f12620a95cd2f50497c5ef704944467d95f6be54
Up <upcraftedlp@gmail.com>
2023-11-30 20:19:22 +0100

    update jam23 page

--- src/routes/jams/2023/+page.svelte
+++ src/routes/jams/2023/+page.svelte
@@ -1,9 +1,29 @@
 <h1>Work in Progress!</h1>
 <h2>Check back later!</h2>
 
-<h4>NOTICE!!! Work on world generation first!</h4>
-<span>
-	For mods that intend on including world generation, we STRONGLY recommend you work on that first
-	(or as soon as possible) so that it can be tested and generated when we set up the server and
-	pack. The remainder of content can be added later on if required.
-</span>
+<section id="info" class="mt-12">
+	<div class="p-5 bg-black bg-opacity-30 rounded-xl">
+		<h4>ℹ Notice: Work on world generation first!</h4>
+		<span>
+			For mods that intend on including world generation, we STRONGLY recommend you work on that
+			first (or as soon as possible) so that it can be tested and generated when we set up the
+			server and pack. The remainder of content can be added later on if required.
+		</span>
+	</div>
+</section>
+
+<section id="links" class="flex flex-col items-center text-3xl">
+	<div class="m-5">
+		<h2>Rules</h2>
+		<a href="/rules" class="underline hover:text-important focus:text-important">
+			Click here for the rules
+		</a>
+	</div>
+	<hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-70 w-9/12" />
+	<div>
+		<h2>FAQ</h2>
+		<a href="/faq" class="underline hover:text-important focus:text-important">
+			Click here for the FAQ
+		</a>
+	</div>
+</section>
