80d25e291dee4e6ce2d27787585eafee65787f4b
Up <upcraftedlp@gmail.com>
2023-12-16 09:55:18 +0100

    add resubmission label

--- src/components/ModEntry.svelte
+++ src/components/ModEntry.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
-	import type { ModData } from "$lib/mods";
-	import type { ModSubmission } from "$lib/submissions";
-    import humanFormat from "human-format";
+	import type { ModData } from '$lib/mods';
+	import type { ModSubmission } from '$lib/submissions';
+	import { Label } from 'flowbite-svelte';
+	import humanFormat from 'human-format';
 
-    export let data: ModSubmission;
-    const mod: ModData = data.mod!;
+	export let data: ModSubmission;
+	const mod: ModData = data.mod!;
 
-    const formattedDownloads = humanFormat(mod.downloads, {
-        maxDecimals: 1,
-        separator: '',
-    });
+	const formattedDownloads = humanFormat(mod.downloads, {
+		maxDecimals: 1,
+		separator: ''
+	});
 </script>
 
 <a
@@ -17,20 +18,26 @@
 	target="_blank"
 	rel="noopener noreferrer"
 	title={mod.name}
-	class="bg-white dark:bg-gray-800 text-gray-500 dark:text-gray-400 rounded-lg border border-gray-200 dark:border-gray-700 divide-gray-200 dark:divide-gray-700 shadow-md flex max-w-sm md:max-w-xl hover:bg-gray-100 dark:hover:bg-gray-700 mb-4 flex-col p-6 items-center gap-2 md:flex-row"
+	class="flex flex-col items-center max-w-sm gap-2 p-6 mb-4 text-gray-500 bg-white border border-gray-200 divide-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:text-gray-400 dark:border-gray-700 dark:divide-gray-700 md:max-w-xl hover:bg-gray-100 dark:hover:bg-gray-700 md:flex-row"
 >
-	<img
-		src={mod.logoUrl}
-		alt={`${mod.name} logo`}
-		class="p-1 ring-2 ring-gray-300 dark:ring-gray-500 w-32 h-32 bg-gray-100 dark:bg-gray-600 text-gray-600 dark:text-gray-300 rounded-full m-auto"
-		loading="lazy"
-	/>
-	<div class="max-w-sm flex items-center flex-col leading-tight">
+	<div class="relative flex items-center">
+		<img
+			src={mod.logoUrl}
+			alt={`${mod.name} logo`}
+			class="w-32 h-32 p-1 m-auto text-gray-600 bg-gray-100 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 dark:bg-gray-600 dark:text-gray-300"
+			loading="lazy"
+		/>
+		{#if data.resubmission}
+			<Label class="-top-2 text-xs absolute bg-sky-950 text-primary py-0.5 px-1 h-5 rounded-sm -left-20">Resubmission</Label>
+		{/if}
+	</div>
+
+	<div class="flex flex-col items-center max-w-sm leading-tight">
 		<h2 class="tracking-wide">{mod.name}</h2>
-		<span class="inline-block text-center tracking-tight">
+		<span class="inline-block tracking-tight text-center">
 			{mod.summary}
-            <br>
-            ({formattedDownloads} Downloads)
+			<br />
+			({formattedDownloads} Downloads)
 		</span>
 	</div>
 </a>
@@ -41,4 +48,3 @@
     <h2>{mod.name}</h2>
     <p>{mod.summary}</p>
 </div> -->
-
