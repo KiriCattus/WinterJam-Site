f4e1510e30795f0e8213108f32174fae4d10a7e5
Up <upcraftedlp@gmail.com>
2023-12-24 20:48:22 +0100

    update jam page notice with MC pack and server

--- src/routes/jams/2023/+page.svelte
+++ src/routes/jams/2023/+page.svelte
@@ -4,6 +4,7 @@
 	import { Tooltip } from 'flowbite-svelte';
 	import Countdown from '$components/Countdown.svelte';
 	import ModEntry from '$components/ModEntry.svelte';
+	import ExternalLink from '$components/ExternalLink.svelte';
 	import type { PageData } from './$types';
 
 	const jamStartDate = new Date('2023-12-01T00:00:00Z');
@@ -52,11 +53,12 @@
 
 <section id="info" class="mt-12">
 	<div class="p-5 bg-black bg-opacity-30 rounded-xl">
-		<h4><InfoCircleSolid class="inline w-8 h-8 mb-1 mr-1 text-blue-500 rounded-full bg-primary" /> Notice: Work on world generation first!</h4>
+		<h4><InfoCircleSolid class="inline w-8 h-8 mb-1 mr-1 text-blue-500 rounded-full bg-primary" />Notice: The Modpack and Minecraft Server are now live!</h4>
 		<span>
-			For mods that intend on including world generation, we STRONGLY recommend you work on that
-			first (or as soon as possible) so that it can be tested and generated when we set up the
-			server and pack. The remainder of content can be added later on if required.
+			We've now opened the doors on our yearly Minecraft Server, and the Modpack is now <ExternalLink href="https://curseforge.com/minecraft/modpacks/winterjam-2023" class="underline">available for download on CurseForge!</ExternalLink><br>
+			To join the server, you will need to be part of our <ExternalLink href="/discord" class="underline">Discord Community</ExternalLink> and whitelist yourself there.<br>
+			<br>
+			Also note that the server is running on Minecraft 1.20.1, Forge 47.1.3. If you want your submission to be playable on the server, you'll need to make sure it's compatible with that version.<br>
 		</span>
 	</div>
 </section>
@@ -85,7 +87,7 @@
 
 <section id="entries">
 	<h2>Entries</h2>
-	<ul class="m-auto mt-8 w-full flex flex-col items-center justify-center">
+	<ul class="flex flex-col items-center justify-center w-full m-auto mt-8">
 		{#each data.mods as mod, i}
 			<li class="min-h-max">
 				<ModEntry data={mod} />
