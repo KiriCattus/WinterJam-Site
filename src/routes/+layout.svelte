852db5ef5feef70ab1c7d7649a5ce6500a96388c
KiriCattus <kiricattus@gmail.com>
2024-12-31 19:59:02 +0000

    Add the submissions link to the navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,6 +20,7 @@
 			<NavLi href="/">Home</NavLi>
 			<NavLi href="/rules">Rules</NavLi>
 			<NavLi href="/faq">FAQ</NavLi>
+			<NavLi href="/jams/2024/submit">Submit a Project</NavLi>
 			<NavLi class="cursor-pointer">
 				Jams<ChevronDownOutline class="inline w-3 h-3 ml-2 text-primary-800 dark:text-white" />
 			</NavLi>
