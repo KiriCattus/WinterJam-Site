1f291ee34b8b0f532071a3a28d91c653615c1c8c
Up <upcraftedlp@gmail.com>
2025-01-01 21:00:21 +0100

    add discord icon for navbar link

--- src/components/ExternalLink.svelte
+++ src/components/ExternalLink.svelte
@@ -6,11 +6,22 @@
 
 	let className: string | undefined = undefined;
 	export { className as class };
+	export let noIcon: boolean = false;
 
 	import { ArrowUpRightFromSquareOutline } from 'flowbite-svelte-icons';
 </script>
 
-<a {href} {target} {rel} class={classNames("inline-flex items-center hover:underline focus:underline h-min hover:text-important focus:text-important", className)}>
+<a
+	{href}
+	{target}
+	{rel}
+	class={classNames(
+		'inline-flex items-center hover:underline focus:underline h-min hover:text-important focus:text-important',
+		className
+	)}
+>
 	<slot />
-	<ArrowUpRightFromSquareOutline class="w-4 h-4 ml-1" tabindex="-1" />
+	{#if !noIcon}
+		<ArrowUpRightFromSquareOutline class="w-4 h-4 ml-1" tabindex="-1" />
+	{/if}
 </a>
