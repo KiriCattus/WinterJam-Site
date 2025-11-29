c532b5e4a39c00d3143047e988dfdf0070828466
Up <10714589+UpcraftLP@users.noreply.github.com>
2025-11-29 08:03:19 +0100

    configure devtools plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
+import devToolsJson from 'vite-plugin-devtools-json';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
-        enhancedImages(),
         tailwindcss(),
-        sveltekit()
+        enhancedImages(),
+        sveltekit(),
+        devToolsJson()
     ],
 	ssr: {
         noExternal: ["@tsparticles/engine", "@tsparticles/slim", "@tsparticles/svelte"], // add all tsparticles libraries here, they're not made for SSR, they're client only
