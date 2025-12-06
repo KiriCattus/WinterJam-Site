a3d66b5381996f9ae7e4604cf7667f906a2711a4
KiriCattus <19393068+KiriCattus@users.noreply.github.com>
2025-12-06 08:23:50 +0000

    Revert test commit

--- src/routes/jams/2025/+page.server.ts
+++ src/routes/jams/2025/+page.server.ts
@@ -2,7 +2,7 @@ import submissions from "$lib/server/submissions";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ platform }) => {
-    const mods = await submissions.getSubmissions(platform, 2024);
+    const mods = await submissions.getSubmissions(platform, 2025);
 
     return {
         mods,
