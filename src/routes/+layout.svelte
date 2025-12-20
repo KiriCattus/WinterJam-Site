fab35536178b171c9eb82b1bd124169025560dbd
KiriCattus <19393068+KiriCattus@users.noreply.github.com>
2025-12-20 20:44:13 +0000

    Add the 2025 submissions page to the navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 
 	$: activeUrl = $page.url.pathname;
 
-	const years = ['2024', '2023', '2022', '2021', '2020', '2019', '2018'];
+	const years = ['2025', '2024', '2023', '2022', '2021', '2020', '2019', '2018'];
 	const newsYears = ['2024', '2023'];
 	const currentYear = new Date().getFullYear();
 </script>
