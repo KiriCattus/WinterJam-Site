c359f89b5f431f041bc831fcf06343e8f7ca8a67
KiriCattus <19393068+KiriCattus@users.noreply.github.com>
2025-12-27 14:39:24 +0000

    Fix attempt 2

--- src/components/Sponsor.svelte
+++ src/components/Sponsor.svelte
@@ -15,14 +15,14 @@
 	{rel}
 	title={name}
 	class={classNames(
-		'bg-white dark:bg-gray-800 text-gray-500 dark:text-gray-400 bg-opacity-30 border border-gray-200 dark:border-gray-700 divide-gray-200 dark:divide-gray-700 shadow-md flex max-w-sm md:max-w-xl hover:bg-gray-100 dark:hover:bg-gray-700 mb-4 flex-col p-6 items-center gap-2',
+		'bg-white dark:bg-gray-800 text-gray-500 dark:text-gray-400 border border-gray-200 dark:border-gray-700 divide-gray-200 dark:divide-gray-700 shadow-md flex max-w-sm md:max-w-xl hover:bg-gray-100 dark:hover:bg-gray-700 mb-4 flex-col p-6 items-center gap-2',
 		reverse ? 'md:flex-row-reverse' : 'md:flex-row'
 	)}
 >
 	<enhanced:img
 		src={logo}
 		alt={name}
-		class="p-1 ring-2 ring-gray-300 dark:ring-gray-500 w-32 h-32 bg-gray-100 dark:bg-gray-600 text-gray-600 dark:text-gray-300 rounded-full m-auto"
+		class="p-1 ring-2 ring-gray-300 dark:ring-gray-500 w-32 h-32 bg-gray-100 dark:bg-gray-600 text-gray-600 dark:text-gray-300 rounded-full bg-opacity-30 m-auto"
 		loading="lazy"
 	/>
 	<div class="max-w-sm flex items-center flex-col leading-tight">
